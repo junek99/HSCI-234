30d7713119ecdc46ff401223ecbd66647630a18a
junek99 <123991621+junek99@users.noreply.github.com>
2023-03-03 02:18:27 -0800

    Update main.py

--- wk6/main.py
+++ wk6/main.py
@@ -2,7 +2,8 @@ import js
 p5 = js.window
 
 program_timer = p5.millis()
-program_state = draw_ice()
+program_state = 'state1'
+
 
 def setup():
     p5.createCanvas(300, 300)  
@@ -28,34 +29,54 @@ def draw_water():
     p5.ellipse(40,8,80,18)
     p5.pop()
 
+def draw_something():
+    p5.push()
+    p5.translate(120,165)
+    p5.ellipse(0,0,100,100)
+    p5.pop()
+
 
 def draw():
     p5.background(255)           
     p5.text(str(p5.mouseX) + ", " + str(p5.mouseY), 10, 15)
-  
 
-    msec = p5.millis()  # get running time in milliseconds
-    sec = int(msec / 1000)  # calculate time in seconds
+    global program_state, program_timer
+
+    msec = p5.millis()  
+    sec = int(msec / 1000) 
     p5.fill(0)
     p5.text('milliseconds: ' + str(msec), 10, 20)
     p5.text('seconds: ' + str(sec), 10, 35)
 
+
+
     if(p5.millis() % 2000 < 500):
-        program_state = draw_water()
+        program_state = 'state1'
     else:
-        program_state = draw_ice()
-        
+        program_state = 'state2'
+
+    if program_state == 'state1':
+        draw_ice()
+    elif program_state == 'state2':
+        draw_water()
+    elif program_state == 'state3':
+        draw_something()
+
 
 
 
 def keyPressed(event):
     print('keyPressed.. ' + str(p5.key))
+    global program_state
+    if program_state == 'state1':
+        program_state = 'state3'
 
 def keyReleased(event):
     print('keyReleased.. ' + str(p5.key))
 
 def mousePressed(event):
     print('mousePressed..')
+
 
 def mouseReleased(event):
     print('mouseReleased..')
