9f47bc9bd4320f1b00f27652d318a604ca370a7f
junek99 <123991621+junek99@users.noreply.github.com>
2023-03-17 01:09:09 -0700

    Update main.py

--- wk8/main.py
+++ wk8/main.py
@@ -1,7 +1,28 @@
 import js
 p5 = js.window
 
+program_state = 'START'
+score = 0
 
+class Space:
+    def __init__(self, x=150, y=250):
+        self.x = x  
+        self.y = y  
+        self.speed = 1
+        self.direction = 1 # From right to left
+
+    def draw(self):
+        self.x -= self.speed 
+        if self.x < -300: # '-' because it moves from right to left   
+            self.x = -150 
+        for i in range(12): 
+            d = self.x + i*50
+            p5.push()
+            p5.translate(d, self.y)
+            p5.fill(112,116,165)
+            p5.rect(0,0,10,300)
+            p5.pop()
+            
 class Spaceship:  
     def __init__(self, x=150, y=250):
         self.img = p5.loadImage('img_test.png')
@@ -33,8 +54,9 @@ class Asteroids:
 
 
         
-spaceship = Spaceship(50,150)
+space = Space(0,0) 
 asteroids = Asteroids(300,150)
+spaceship = Spaceship(50,150)
 
 
 def setup():
@@ -42,36 +64,47 @@ def setup():
     print('finished setup') 
     
 def draw():
-    p5.background(255)  
-    if p5.keyIsPressed:
-        if p5.keyCode == p5.UP_ARROW:
-            if spaceship.y > 0:
-                spaceship.y -= spaceship.speed
-        elif p5.keyCode == p5.DOWN_ARROW:
-            if spaceship.y < p5.height - 10:
-                spaceship.y += spaceship.speed
-        elif p5.keyCode == p5.LEFT_ARROW:
-            if spaceship.x > 0:
-                spaceship.x -= spaceship.speed
-        elif p5.keyCode == p5.RIGHT_ARROW:
-            if spaceship.x < p5.width - 20:
-                spaceship.x += spaceship.speed
-                
-    spaceship.draw()
-    asteroids.draw()
+    global program_state, score
 
+    if program_state == 'START': # Start Screen
+        p5.background(0)  
+        p5.fill(255)
+        p5.textFont('Comic Sans MS', 30)
+        p5.text('Asteroid Attack', 35, 100)
+        p5.textFont('Comic Sans MS', 18)
+        p5.text('click SHIFT to start', 65, 180)
+    elif program_state == 'PLAY': # Game Play
+        p5.background(141,143,181)  
+        if p5.keyIsPressed:
+            if p5.keyCode == p5.UP_ARROW:
+                if spaceship.y > 0:
+                    spaceship.y -= spaceship.speed
+            elif p5.keyCode == p5.DOWN_ARROW:
+                if spaceship.y < p5.height - 10:
+                    spaceship.y += spaceship.speed
+            elif p5.keyCode == p5.LEFT_ARROW:
+                if spaceship.x > 0:
+                    spaceship.x -= spaceship.speed
+            elif p5.keyCode == p5.RIGHT_ARROW:
+                if spaceship.x < p5.width - 20:
+                    spaceship.x += spaceship.speed
+        
+        space.draw()
+        asteroids.draw()
+        spaceship.draw()
 
+        p5.textFont('Comic Sans MS', 10)
+        p5.text('Score: ' + str(score), 10, 20)
+        
 def keyPressed(event):
-    pass 
-
+    global program_state
+    if p5.keyCode == p5.SHIFT:
+        if program_state == 'START':
+            program_state = 'PLAY'  
 
 def keyReleased(event):
     pass
 
 
 def mousePressed(event):
-    pass
-
-
-def mouseReleased(event):
     pass
