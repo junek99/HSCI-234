f8b2e23e4b5081b4ff1a2db2a6375a4bd52b65be
junek99 <123991621+junek99@users.noreply.github.com>
2023-03-16 23:09:41 -0700

    Update main.py

--- wk8/main.py
+++ wk8/main.py
@@ -1,20 +1,41 @@
 import js
 p5 = js.window
 
+
 class Spaceship:  
     def __init__(self, x=150, y=250):
+        self.img = p5.loadImage('img_test.png')
         self.x = x  
         self.y = y  
         self.speed = 3
-
+        
+        
     def draw(self):
         p5.push()
         p5.translate(self.x, self.y)
-        p5.fill(0)
-        p5.rect(0, 0, 20, 10)
+        p5.image(self.img, 0, 0)
         p5.pop()
 
-spaceship = Spaceship(20, 150)
+class Asteroids:
+    def __init__(self, x = 0, y = 0):
+        self.x = x  
+        self.y = y  
+        self.size = p5.random(10, 30)
+        self.speed = p5.random(1, 5) 
+        self.direction = 1  
+     
+    def draw(self):
+        p5.push()
+        p5.translate(self.x, self.y)
+        p5.rect(0,0, self.size, self.size)
+        p5.pop()
+        self.x -= self.speed * self.direction
+
+
+        
+spaceship = Spaceship(50,150)
+asteroids = Asteroids(300,150)
+
 
 def setup():
     p5.createCanvas(300, 300) 
@@ -36,16 +57,21 @@ def draw():
             if spaceship.x < p5.width - 20:
                 spaceship.x += spaceship.speed
                 
-    spaceship.draw() 
+    spaceship.draw()
+    asteroids.draw()
+
 
 def keyPressed(event):
     pass 
 
+
 def keyReleased(event):
     pass
 
+
 def mousePressed(event):
     pass
+
 
 def mouseReleased(event):
     pass
